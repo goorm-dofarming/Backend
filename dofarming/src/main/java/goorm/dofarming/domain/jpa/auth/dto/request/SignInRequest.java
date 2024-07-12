9c93f07851701228bfb5261c70b9df89d66ff7db
mo_o.noe <hyeonhoi11@naver.com>
2024-07-12 22:59:40 +0900

    feat: DTO별 swagger 적용

--- dofarming/src/main/java/goorm/dofarming/domain/jpa/auth/dto/request/SignInRequest.java
+++ dofarming/src/main/java/goorm/dofarming/domain/jpa/auth/dto/request/SignInRequest.java
@@ -1,14 +1,19 @@
 package goorm.dofarming.domain.jpa.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "로그인 요청 정보를 담는 DTO")
 public record SignInRequest(
+
+        @Schema(description = "사용자 이메일", example = "user@example.com")
         @Email
         @NotEmpty(message = "이메일은 필수 항목입니다.")
         String email,
 
+        @Schema(description = "사용자 비밀번호", example = "userpassword")
         @NotEmpty(message = "비밀번호는 필수 항목입니다.")
         String password
 ) {
--- dofarming/src/main/java/goorm/dofarming/domain/jpa/email/dto/request/EmailRequest.java
+++ dofarming/src/main/java/goorm/dofarming/domain/jpa/email/dto/request/EmailRequest.java
@@ -1,10 +1,14 @@
 package goorm.dofarming.domain.jpa.email.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+@Schema(description = "이메일 요청 정보를 담는 DTO")
 public record EmailRequest(
+
+        @Schema(description = "사용자 이메일", example = "user@example.com")
         @Email
         @NotEmpty(message = "이메일은 필수 항목입니다.")
         String email
