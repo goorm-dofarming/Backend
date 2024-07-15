457947f7a70fae7d9c6ac1fe9f0ddd69472336fb
githublees <leeho_@naver.com>
2024-07-15 23:54:17 +0900

    feat: 채팅방 입장/퇴장 기능 구현

--- dofarming/src/main/java/goorm/dofarming/domain/jpa/chatroom/service/ChatroomService.java
+++ dofarming/src/main/java/goorm/dofarming/domain/jpa/chatroom/service/ChatroomService.java
@@ -3,7 +3,10 @@ package goorm.dofarming.domain.jpa.chatroom.service;
 import goorm.dofarming.domain.jpa.chatroom.dto.request.ChatroomCreateRequest;
 import goorm.dofarming.domain.jpa.chatroom.entity.Chatroom;
 import goorm.dofarming.domain.jpa.chatroom.repository.ChatroomRepository;
+import goorm.dofarming.domain.jpa.join.entity.Join;
+import goorm.dofarming.domain.jpa.join.repository.JoinRepository;
 import goorm.dofarming.domain.jpa.user.entity.User;
+import goorm.dofarming.domain.jpa.user.repository.UserRepository;
 import goorm.dofarming.global.common.entity.Status;
 import goorm.dofarming.global.common.error.ErrorCode;
 import goorm.dofarming.global.common.error.exception.CustomException;
@@ -17,6 +20,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatroomService {
 
     private final ChatroomRepository chatroomRepository;
+    private final JoinRepository joinRepository;
+    private final UserRepository userRepository;
 
     @Transactional
     public Long createRoom(ChatroomCreateRequest chatroomCreateRequest) {
@@ -31,8 +36,30 @@ public class ChatroomService {
         chatroom.delete();
     }
 
+    @Transactional
+    public Long joinRoom(Long userId, Long roomId) {
+        User user = existByUserId(userId);
+        Chatroom chatroom = existByRoomId(roomId);
+
+        Join join = Join.join(user, chatroom);
+        Join saveJoin = joinRepository.save(join);
+        return saveJoin.getJoinId();
+    }
+
+    @Transactional
+    public void leaveRoom(Long userId, Long roomId) {
+        Join join = joinRepository.findByUser_UserIdAndChatroom_RoomIdAndStatus(userId, roomId, Status.ACTIVE)
+                .orElseThrow(() -> new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "입장한 채팅방이 아닙니다."));
+        join.delete();
+    }
+
     private Chatroom existByRoomId(Long roomId) {
         return chatroomRepository.findByRoomIdAndStatus(roomId, Status.ACTIVE)
                 .orElseThrow(() -> new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "Room not found."));
+    }
+
+    private User existByUserId(Long userId) {
+        return userRepository.findByUserIdAndStatus(userId, Status.ACTIVE)
+                .orElseThrow(() -> new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "User not found."));
     }
 }
