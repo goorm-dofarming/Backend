0ad6612ca8c8371f4b27a5569a64a13e19a8234a
RealTone <realtone98@gmail.com>
2024-07-16 14:31:39 +0900

    feat: https healthy controller

---
+++ dofarming/src/main/java/goorm/dofarming/infra/https/HealthCheckController.java
@@ -0,0 +1,14 @@
+package goorm.dofarming.infra.https;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String healthCheck() {
+        return "OK";
+    }
+
+}
