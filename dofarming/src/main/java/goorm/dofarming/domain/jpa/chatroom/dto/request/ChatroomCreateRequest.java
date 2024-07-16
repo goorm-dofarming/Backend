25931879f8994521dfa744b5938c4ec09d677e59
mo_o.noe <hyeonhoi11@naver.com>
2024-07-16 18:09:51 +0900

    feat: DTO별 swagger 적용(7/16기준)

--- dofarming/src/main/java/goorm/dofarming/domain/jpa/chatroom/dto/request/ChatroomCreateRequest.java
+++ dofarming/src/main/java/goorm/dofarming/domain/jpa/chatroom/dto/request/ChatroomCreateRequest.java
@@ -1,9 +1,13 @@
 package goorm.dofarming.domain.jpa.chatroom.dto.request;
 
 import goorm.dofarming.domain.jpa.chatroom.entity.Region;
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "채팅방 생성 요청 정보를 담는 DTO")
 public record ChatroomCreateRequest(
+        @Schema(description = "채팅방 제목", example = "New Chatroom")
         String title,
+        @Schema(description = "채팅방 지역", example = "SEOUL")
         Region region
 ) {
 }
