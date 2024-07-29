c3cd8062a8fac2351d03513393aa00dbebe9a90d
RealTone <realtone98@gmail.com>
2024-07-29 17:12:23 +0900

    feat: likeList 반환 기능 추가

--- dofarming/src/main/java/goorm/dofarming/domain/jpa/like/controller/LikeController.java
+++ dofarming/src/main/java/goorm/dofarming/domain/jpa/like/controller/LikeController.java
@@ -7,9 +7,12 @@ import goorm.dofarming.global.common.error.ErrorCode;
 import goorm.dofarming.global.common.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -29,4 +32,11 @@ public class LikeController {
         return likeService.like(userId, placeId, dataType);
     }
 
+    @GetMapping("/likeList")
+    private List<?> likeList(@AuthenticationPrincipal DofarmingUserDetails user) {
+        if (user == null) throw new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "회원정보가 일치하지 않습니다.");
+        Long userId = user.getUserId();
+
+        return likeService.getLikeList(userId);
+    }
 }
