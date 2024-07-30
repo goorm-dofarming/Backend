3f67a60e2296a657729940b51a19bbf48c93ff98
mo_o.noe <hyeonhoi11@naver.com>
2024-07-31 00:04:38 +0900

    좋아요, 로그, 추천 api swagger 적용

--- dofarming/src/main/java/goorm/dofarming/domain/jpa/like/controller/LikeController.java
+++ dofarming/src/main/java/goorm/dofarming/domain/jpa/like/controller/LikeController.java
@@ -4,7 +4,15 @@ import goorm.dofarming.domain.jpa.like.entity.Like;
 import goorm.dofarming.domain.jpa.like.service.LikeService;
 import goorm.dofarming.global.auth.DofarmingUserDetails;
 import goorm.dofarming.global.common.error.ErrorCode;
+import goorm.dofarming.global.common.error.ErrorResponse;
 import goorm.dofarming.global.common.error.exception.CustomException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +22,30 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Like", description = "Like 관련 API")
 @RestController
 @RequiredArgsConstructor
 public class LikeController {
 
     private final LikeService likeService;
 
+    @Operation(
+            operationId = "like",
+            summary = "좋아요 추가 또는 취소 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리됨", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = Like.class))
+                    }),
+                    @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    })
+            }
+    )
     @PostMapping("/like")
     public Like like(
-            @AuthenticationPrincipal DofarmingUserDetails user,
-            @RequestParam Long placeId,
-            @RequestParam int dataType
+            @Parameter(description = "인증된 사용자 정보") @AuthenticationPrincipal DofarmingUserDetails user,
+            @Parameter(description = "장소 ID") @RequestParam Long placeId,
+            @Parameter(description = "데이터 타입") @RequestParam int dataType
     ) {
         if (user == null) throw new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "회원정보가 일치하지 않습니다.");
         Long userId = user.getUserId();
@@ -32,8 +53,36 @@ public class LikeController {
         return likeService.like(userId, placeId, dataType);
     }
 
+    @Operation(
+            operationId = "likeList",
+            summary = "사용자의 좋아요 목록 조회 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리됨", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class),
+                                    examples = @ExampleObject(
+                                            value = "[\n" +
+                                                    "  {\n" +
+                                                    "    \"id\": 1,\n" +
+                                                    "    \"type\": \"Restaurant\",\n" +
+                                                    "    \"title\": \"용용 선생\",\n" +
+                                                    "    \"addr\": \"서울시 강남구\",\n" +
+                                                    "    \"tel\": \"010-1234-5678\",\n" +
+                                                    "    \"image\": \"http://example.com/image.jpg\",\n" +
+                                                    "    \"mapX\": 127.123456,\n" +
+                                                    "    \"mapY\": 37.123456,\n" +
+                                                    "    \"likeCount\": 7\n" +
+                                                    "  }\n" + "]")
+                            )
+                    }),
+                    @ApiResponse(responseCode = "404", description = "사용자를 찾을 수 없음", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    })
+            }
+    )
     @GetMapping("/likeList")
-    private List<?> likeList(@AuthenticationPrincipal DofarmingUserDetails user) {
+    private List<?> likeList(
+            @Parameter(description = "인증된 사용자 정보") @AuthenticationPrincipal DofarmingUserDetails user
+    ) {
         if (user == null) throw new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "회원정보가 일치하지 않습니다.");
         Long userId = user.getUserId();
 
--- dofarming/src/main/java/goorm/dofarming/domain/jpa/recommend/controller/RecommendController.java
+++ dofarming/src/main/java/goorm/dofarming/domain/jpa/recommend/controller/RecommendController.java
@@ -4,7 +4,15 @@ import goorm.dofarming.domain.jpa.recommend.entity.RecommendDTO;
 import goorm.dofarming.domain.jpa.recommend.service.RecommendService;
 import goorm.dofarming.global.auth.DofarmingUserDetails;
 import goorm.dofarming.global.common.error.ErrorCode;
+import goorm.dofarming.global.common.error.ErrorResponse;
 import goorm.dofarming.global.common.error.exception.CustomException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@Tag(name = "Recommend", description = "Recommend 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/recommend")
@@ -21,23 +28,132 @@ public class RecommendController {
 
     private final RecommendService recommendService;
 
+    @Operation(
+            operationId = "recommendOcean",
+            summary = "바다 테마 추천 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리됨", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = RecommendDTO.class), examples = @ExampleObject(
+                                    value = "{\n" +
+                                            "  \"logId\": 196,\n" +
+                                            "  \"address\": \"경상남도 남해군 남면\",\n" +
+                                            "  \"recommendations\": [\n" +
+                                            "    {\n" +
+                                            "      \"id\": 73,\n" +
+                                            "      \"title\": \"남해 가천 해변과 암수바위\",\n" +
+                                            "      \"addr\": \"경상남도 남해군 남면\",\n" +
+                                            "      \"tel\": \"\",\n" +
+                                            "      \"image\": \"\",\n" +
+                                            "      \"mapX\": 127.8940102692,\n" +
+                                            "      \"mapY\": 34.72648889,\n" +
+                                            "      \"dataType\": 1,\n" +
+                                            "      \"liked\": false,\n" +
+                                            "      \"countLikes\": 0\n" +
+                                            "    },\n" +
+                                            "    {\n" +
+                                            "      \"id\": 5750,\n" +
+                                            "      \"title\": \"남해바다체험센터\",\n" +
+                                            "      \"addr\": \"경상남도 남해군 남면 남면로1229번길 10-34\",\n" +
+                                            "      \"tel\": \"\",\n" +
+                                            "      \"image\": \"http://tong.visitkorea.or.kr/cms/resource/31/2787731_image2_1.jpg\",\n" +
+                                            "      \"mapX\": 127.861079344,\n" +
+                                            "      \"mapY\": 34.7448168207,\n" +
+                                            "      \"dataType\": 3,\n" +
+                                            "      \"liked\": false,\n" +
+                                            "      \"countLikes\": 0\n" +
+                                            "    }\n" +
+                                            "  ]\n" +
+                                            "}"
+                            ))
+                    }),
+                    @ApiResponse(responseCode = "404", description = "회원정보가 일치하지 않음", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    })
+            }
+    )
     @GetMapping("/ocean")
-    public RecommendDTO recommendOcean(@AuthenticationPrincipal DofarmingUserDetails user) {
+    public RecommendDTO recommendOcean(
+            @Parameter(description = "인증된 사용자 정보") @AuthenticationPrincipal DofarmingUserDetails user
+    ) {
         if (user == null) throw new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "회원정보가 일치하지 않습니다.");
         Long userId = user.getUserId();
         return recommendService.recommendOcean(userId);
     }
 
+    @Operation(
+            operationId = "recommendMountain",
+            summary = "산 테마 추천 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리됨", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = RecommendDTO.class), examples = @ExampleObject(
+                                    value = "{\n" +
+                                            "  \"logId\": 198,\n" +
+                                            "  \"address\": \"경상남도\",\n" +
+                                            "  \"recommendations\": [\n" +
+                                            "    {\n" +
+                                            "      \"id\": 150,\n" +
+                                            "      \"title\": \"지리산 국립공원\",\n" +
+                                            "      \"addr\": \"경상남도 함양군 마천면\",\n" +
+                                            "      \"tel\": \"\",\n" +
+                                            "      \"image\": \"\",\n" +
+                                            "      \"mapX\": 127.625633,\n" +
+                                            "      \"mapY\": 35.354141,\n" +
+                                            "      \"dataType\": 2,\n" +
+                                            "      \"liked\": false,\n" +
+                                            "      \"countLikes\": 0\n" +
+                                            "    }\n" +
+                                            "  ]\n" +
+                                            "}"
+                            ))
+                    }),
+                    @ApiResponse(responseCode = "404", description = "회원정보가 일치하지 않음", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    })
+            }
+    )
     @GetMapping("/mountain")
-    public RecommendDTO recommendMountain(@AuthenticationPrincipal DofarmingUserDetails user) {
+    public RecommendDTO recommendMountain(
+            @Parameter(description = "인증된 사용자 정보") @AuthenticationPrincipal DofarmingUserDetails user
+    ) {
         if (user == null) throw new CustomException(ErrorCode.RESOURCE_NOT_FOUND, "회원정보가 일치하지 않습니다.");
         Long userId = user.getUserId();
         return recommendService.recommendMountain(userId);
     }
 
+    @Operation(
+            operationId = "recommendTheme",
+            summary = "테마별 추천 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리됨", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = RecommendDTO.class), examples = @ExampleObject(
+                                    value = "{\n" +
+                                            "  \"logId\": 198,\n" +
+                                            "  \"address\": \"address\",\n" +
+                                            "  \"recommendations\": [\n" +
+                                            "    {\n" +
+                                            "      \"id\": 3464,\n" +
+                                            "      \"title\": \"국립암\",\n" +
+                                            "      \"addr\": \"전라남도 영광군 영광읍 대학길4길 10\",\n" +
+                                            "      \"tel\": \"061-351-2020\",\n" +
+                                            "      \"image\": \"http://tong.visitkorea.or.kr/cms/resource/45/1972345_image2_1.jpg\",\n" +
+                                            "      \"mapX\": 126.4976638514,\n" +
+                                            "      \"mapY\": 35.2758643528,\n" +
+                                            "      \"dataType\": 5,\n" +
+                                            "      \"liked\": false,\n" +
+                                            "      \"countLikes\": 0\n" +
+                                            "    }\n" +
+                                            "  ]\n" +
+                                            "}"
+                            ))
+                    }),
+                    @ApiResponse(responseCode = "404", description = "회원정보가 일치하지 않음", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    })
+            }
+    )
     @GetMapping("/theme")
     public RecommendDTO recommendTheme(
-            @AuthenticationPrincipal DofarmingUserDetails user,
+            @Parameter(description = "인증된 사용자 정보") @AuthenticationPrincipal DofarmingUserDetails user,
             @RequestParam("theme") int dataType,
             @RequestParam("mapX") double mapX,
             @RequestParam("mapY") double mapY,
@@ -48,12 +164,43 @@ public class RecommendController {
         return recommendService.recommendTheme(dataType, mapX, mapY, userId, address);
     }
 
+    @Operation(
+            operationId = "recommendRandom",
+            summary = "랜덤 추천 API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "요청이 성공적으로 처리됨", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = RecommendDTO.class), examples = @ExampleObject(
+                                    value = "{\n" +
+                                            "  \"logId\": 199,\n" +
+                                            "  \"address\": \"address\",\n" +
+                                            "  \"recommendations\": [\n" +
+                                            "    {\n" +
+                                            "      \"id\": 672,\n" +
+                                            "      \"title\": \"영광 융암마을\",\n" +
+                                            "      \"addr\": \"전라남도 영광군 군남면 육창로1길 50-1\",\n" +
+                                            "      \"tel\": \"\",\n" +
+                                            "      \"image\": \"\",\n" +
+                                            "      \"mapX\": 126.4344547522,\n" +
+                                            "      \"mapY\": 35.196750576,\n" +
+                                            "      \"dataType\": 3,\n" +
+                                            "      \"liked\": true,\n" +
+                                            "      \"countLikes\": 1\n" +
+                                            "    }\n" +
+                                            "  ]\n" +
+                                            "}"
+                            ))
+                    }),
+                    @ApiResponse(responseCode = "404", description = "회원정보가 일치하지 않음", content = {
+                            @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    })
+            }
+    )
     @GetMapping("/random")
     public RecommendDTO recommendRandom(
             @RequestParam("mapX") double mapX,
             @RequestParam("mapY") double mapY,
             @RequestParam("address") String address,
-            @AuthenticationPrincipal DofarmingUserDetails user
+            @Parameter(description = "인증된 사용자 정보") @AuthenticationPrincipal DofarmingUserDetails user
     ) {
         if(user != null){
             Long userId = user.getUserId();
