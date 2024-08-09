39b4ec61903c159ac90de3de5797e795c5794cb3
githublees <leeho_@naver.com>
2024-08-09 12:47:34 +0900

    fix: 카프카 토픽 설정 변경

--- dofarming/src/main/java/goorm/dofarming/global/config/kafka/KafkaConstants.java
+++ dofarming/src/main/java/goorm/dofarming/global/config/kafka/KafkaConstants.java
@@ -1,8 +1,8 @@
 package goorm.dofarming.global.config.kafka;
 
 public class KafkaConstants {
-    public static final String CHAT_TOPIC = "chat-log";
-    public static final String PROCESS_CHAT_TOPIC = "process-chat-log";
-    public static final String ALARM_TOPIC = "alarm-log";
+    public static final String CHAT_TOPIC = "chat-log-v2";
+    public static final String PROCESS_CHAT_TOPIC = "process-chat-log-v2";
+    public static final String ALARM_TOPIC = "alarm-log-v2";
     public static final String KAFKA_BROKER = "172.31.7.189:9092,172.31.8.251:9092,172.31.13.117:9092";
 }
--- notification/src/main/java/goorm/notification/config/kafka/KafkaConstants.java
+++ notification/src/main/java/goorm/notification/config/kafka/KafkaConstants.java
@@ -1,6 +1,6 @@
 package goorm.notification.config.kafka;
 
 public class KafkaConstants {
-    public static final String KAFKA_TOPIC = "alarm-log";
+    public static final String KAFKA_TOPIC = "alarm-log-v2";
     public static final String KAFKA_BROKER = "172.31.7.189:9092,172.31.8.251:9092,172.31.13.117:9092";
 }
